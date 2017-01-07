713342797bfd261c513b3f77c7c05a1a73626c41
akondo06 <akondo.ionutz@gmail.com>
2017-01-07 17:48:59 +0100

    header redone

--- cache/views/7625735e8545f5e56aa633df35c574539462e9d2.php
+++ cache/views/7625735e8545f5e56aa633df35c574539462e9d2.php
@@ -1,33 +1,39 @@
 <header>
-		<nav class="navbar navbar-default navbar-with-ad">
-			<div class="container">
-				<div class="navbar-header">
-					<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-						<span class="sr-only">Toggle navigation</span>
-						<span class="icon-bar"></span>
-						<span class="icon-bar"></span>
-						<span class="icon-bar"></span>
-					</button>
-					<a class="navbar-brand navbar-left" href="<?php echo e(urlFor('index')); ?>">Curs Valutar</a>
-					<div class="ad size-728x90 navbar-right"></div>
-				</div>
-				<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-					<ul class="nav navbar-nav navbar-left">
-						<li <?php echo isRoute('index'); ?>><a href="<?php echo e(urlFor('index')); ?>">Acasa</a></li>
-						<li <?php echo isRoute('currency-converter'); ?>><a href="<?php echo e(urlFor('currency-converter')); ?>">Convertor Valutar</a></li>
-						<li <?php echo isRoute('currency-evolution'); ?>><a href="<?php echo e(urlFor('currency-evolution')); ?>">Evolutie Curs</a></li>
-						
-						<li <?php echo isRoute(['history', 'onDate']); ?>><a href="<?php echo e(urlFor('history')); ?>">Istoric Curs Valutar</a></li>
-					</ul>
-					<div class="nav navbar-nav navbar-right todays-rates">
-						<?php $__currentLoopData = ratesOnDate(); $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $rate): $__env->incrementLoopIndices(); $loop = $__env->getFirstLoop(); ?>
-						<div class="currency" title="<?php echo e($rate->currency); ?>">
-							<span class="flag <?php echo e(strtolower($rate->currency)); ?>"></span>
-							<span class="value"><?php echo e(roNumber($rate->converted_value, 2)); ?> RON</span>
-						</div>
-						<?php endforeach; $__env->popLoop(); $loop = $__env->getFirstLoop(); ?>
-					</div>
-				</div>
+	<nav class="navbar navbar-default navbar-main">
+		<div class="container">
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+					<span class="sr-only">Toggle navigation</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+				<a class="navbar-brand navbar-left" href="<?php echo e(urlFor('index')); ?>">Curs Valutar</a>
 			</div>
-		</nav>
+			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+				<ul class="nav navbar-nav navbar-right">
+					<li <?php echo isRoute('index'); ?>><a href="<?php echo e(urlFor('index')); ?>">Acasa</a></li>
+					<li <?php echo isRoute('currency-converter'); ?>><a href="<?php echo e(urlFor('currency-converter')); ?>">Convertor Valutar</a></li>
+					<li <?php echo isRoute('currency-evolution'); ?>><a href="<?php echo e(urlFor('currency-evolution')); ?>">Evolutie Curs</a></li>
+					
+					<li <?php echo isRoute(['history', 'onDate']); ?>><a href="<?php echo e(urlFor('history')); ?>">Istoric Curs Valutar</a></li>
+				</ul>
+			</div>
+		</div>
+	</nav>
+	<nav class="navbar navbar-default navbar-info">
+		<div class="container">
+			<div class="nav navbar-nav navbar-left page-title">
+				<h1>Curs Valutar</h1>
+			</div>
+			<div class="nav navbar-nav navbar-right todays-rates">
+				<?php $__currentLoopData = ratesOnDate(); $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $rate): $__env->incrementLoopIndices(); $loop = $__env->getFirstLoop(); ?>
+				<div class="currency" title="<?php echo e($rate->currency); ?>">
+					<span class="flag <?php echo e(strtolower($rate->currency)); ?>"></span>
+					<span class="value"><?php echo e(roNumber($rate->converted_value, 2)); ?> RON</span>
+				</div>
+				<?php endforeach; $__env->popLoop(); $loop = $__env->getFirstLoop(); ?>
+			</div>
+		</div>
+	</nav>
 </header>
--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,42 +1,48 @@
 <header>
-		<nav class="navbar navbar-default navbar-with-ad">
-			<div class="container">
-				<div class="navbar-header">
-					<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-						<span class="sr-only">Toggle navigation</span>
-						<span class="icon-bar"></span>
-						<span class="icon-bar"></span>
-						<span class="icon-bar"></span>
-					</button>
-					<a class="navbar-brand navbar-left" href="{{urlFor('index')}}">Curs Valutar</a>
-					<div class="ad size-728x90 navbar-right"></div>
-				</div>
-				<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-					<ul class="nav navbar-nav navbar-left">
-						<li {!!isRoute('index')!!}><a href="{{urlFor('index')}}">Acasa</a></li>
-						<li {!!isRoute('currency-converter')!!}><a href="{{urlFor('currency-converter')}}">Convertor Valutar</a></li>
-						<li {!!isRoute('currency-evolution')!!}><a href="{{urlFor('currency-evolution')}}">Evolutie Curs</a></li>
-						{{-- <li><a href="{{urlFor('page', ['name' => 'curs-valutar-banci'])}}">Curs Valutar Banci</a></li>
-						<li class="dropdown">
-							<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Cel mai bun curs <span class="caret"></span></a>
-							<ul class="dropdown-menu">
-								<li><a href="{{urlFor('page', ['name' => 'cel-mai-bun-curs-euro'])}}">EURO</a></li>
-								<li><a href="{{urlFor('page', ['name' => 'cel-mai-bun-curs-dolar'])}}">USD - Dolar American</a></li>
-								<li><a href="{{urlFor('page', ['name' => 'cel-mai-bun-curs-lira-sterlina'])}}">GBP - Lira Sterlina</a></li>
-								<li><a href="{{urlFor('page', ['name' => 'cel-mai-bun-curs-franc-elvetian'])}}">CHF - Francul Elvetian</a></li>
-							</ul>
-						</li> --}}
-						<li {!!isRoute(['history', 'onDate'])!!}><a href="{{urlFor('history')}}">Istoric Curs Valutar</a></li>
-					</ul>
-					<div class="nav navbar-nav navbar-right todays-rates">
-						@foreach (ratesOnDate() as $rate)
-						<div class="currency" title="{{$rate->currency}}">
-							<span class="flag {{strtolower($rate->currency)}}"></span>
-							<span class="value">{{roNumber($rate->converted_value, 2)}} RON</span>
-						</div>
-						@endforeach
-					</div>
-				</div>
+	<nav class="navbar navbar-default navbar-main">
+		<div class="container">
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+					<span class="sr-only">Toggle navigation</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+				<a class="navbar-brand navbar-left" href="{{urlFor('index')}}">Curs Valutar</a>
 			</div>
-		</nav>
+			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+				<ul class="nav navbar-nav navbar-right">
+					<li {!!isRoute('index')!!}><a href="{{urlFor('index')}}">Acasa</a></li>
+					<li {!!isRoute('currency-converter')!!}><a href="{{urlFor('currency-converter')}}">Convertor Valutar</a></li>
+					<li {!!isRoute('currency-evolution')!!}><a href="{{urlFor('currency-evolution')}}">Evolutie Curs</a></li>
+					{{-- <li><a href="{{urlFor('page', ['name' => 'curs-valutar-banci'])}}">Curs Valutar Banci</a></li>
+					<li class="dropdown">
+						<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Cel mai bun curs <span class="caret"></span></a>
+						<ul class="dropdown-menu">
+							<li><a href="{{urlFor('page', ['name' => 'cel-mai-bun-curs-euro'])}}">EURO</a></li>
+							<li><a href="{{urlFor('page', ['name' => 'cel-mai-bun-curs-dolar'])}}">USD - Dolar American</a></li>
+							<li><a href="{{urlFor('page', ['name' => 'cel-mai-bun-curs-lira-sterlina'])}}">GBP - Lira Sterlina</a></li>
+							<li><a href="{{urlFor('page', ['name' => 'cel-mai-bun-curs-franc-elvetian'])}}">CHF - Francul Elvetian</a></li>
+						</ul>
+					</li> --}}
+					<li {!!isRoute(['history', 'onDate'])!!}><a href="{{urlFor('history')}}">Istoric Curs Valutar</a></li>
+				</ul>
+			</div>
+		</div>
+	</nav>
+	<nav class="navbar navbar-default navbar-info">
+		<div class="container">
+			<div class="nav navbar-nav navbar-left page-title">
+				<h1>Curs Valutar</h1>
+			</div>
+			<div class="nav navbar-nav navbar-right todays-rates">
+				@foreach (ratesOnDate() as $rate)
+				<div class="currency" title="{{$rate->currency}}">
+					<span class="flag {{strtolower($rate->currency)}}"></span>
+					<span class="value">{{roNumber($rate->converted_value, 2)}} RON</span>
+				</div>
+				@endforeach
+			</div>
+		</div>
+	</nav>
 </header>
